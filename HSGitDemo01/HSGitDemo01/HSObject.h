7145e38086f0d1e016e6c5d02a66bbbfb7c51ffc
wangze <apple@wxz.local>
2016-04-20 00:17:00 +0800

    加了一个HSObject类

---
+++ HSGitDemo01/HSGitDemo01/HSObject.h
@@ -0,0 +1,13 @@
+//
+//  HSObject.h
+//  HSGitDemo01
+//
+//  Created by wangze on 16/4/20.
+//  Copyright © 2016年 huashan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HSObject : NSObject
+
+@end
---
+++ HSGitDemo01/HSGitDemo01/HSObject.m
@@ -0,0 +1,13 @@
+//
+//  HSObject.m
+//  HSGitDemo01
+//
+//  Created by wangze on 16/4/20.
+//  Copyright © 2016年 huashan. All rights reserved.
+//
+
+#import "HSObject.h"
+
+@implementation HSObject
+
+@end
